78ff8f917920366479c4d54a229f96cb0b17f1d4
emrepbu <emrewindowsarg@gmail.com>
2025-06-23 19:34:25 +0300

    Initial Commit

---
+++ FeatureiOSLiveActivity/FeatureiOSLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureiOSLiveActivityApp.swift
+//  FeatureiOSLiveActivity
+//
+//  Created by emre argana on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FeatureiOSLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
